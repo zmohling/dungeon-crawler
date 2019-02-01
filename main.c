67a2edab1ab6265922c2f5e01bbd0ae268b34acc
Zachary Mohling <zmohling@gmail.com>
2019-01-31 18:41:32 -0600

    Add random room generation

--- main.c
+++ main.c
@@ -9,21 +9,40 @@ typedef struct Room {
 } Room;
 
 /* Prototypes */
-void draw_dungeon(char *, Room *);
-void generate_rooms(Room *);
+void draw_dungeon(char *, Room *, int, int);
+void generate_rooms(Room *, int, int, int);
 int intersects(Room, Room);
+int out_of_bounds(Room, int, int);
+
+/* Debug vars */
+int attempts_to_generate = 0;
 
 int main(int argc, char *argv[])
 {
-    char dungeon[21][80];
-    Room rooms[6];
+    int x = 80;
+    int y = 21;
+    int MAX_ROOMS = 6;
 
-    draw_dungeon(dungeon, rooms);
+    char dungeon[y][x];
+    Room rooms[MAX_ROOMS];
 
+    /* Generate rooms */
+    generate_rooms(rooms, MAX_ROOMS, x, y); 
+    draw_dungeon(dungeon, rooms, x, y);
+
+    /*
+    int i;
+    for (i = 0; i < MAX_ROOMS; i++)
+    {
+        printf("Room %d: (%d, %d) -> (%d, %d)\n", i, rooms[i].x1, rooms[i].y1, rooms[i].x2, rooms[i].y2);
+    }
+    */
+
+    printf("Attempts to generate: %d\n", attempts_to_generate);
     return 0;
 }
 
-void draw_dungeon(char *dungeon, Room *rooms)
+void draw_dungeon(char *dungeon, Room *rooms, int x, int y)
 {
     int i, j;
     for (i = 0; i < 21; i++)
@@ -31,42 +50,52 @@ void draw_dungeon(char *dungeon, Room *rooms)
         for (j = 0; j < 80; j++)
         {
             char *p;
-            p = &(dungeon + (i * 80) + j)
+            p = (dungeon + (i * 80) + j);
             
-            *(dungeon + (i * 80) + j) = ' ';
+            *(p) = ' ';
 
             if (j == 0 || j == 79)
             {
-                *(dungeon + (i * 80) + j) = '|';
+                *(p) = '|';
             }
 
             if (i == 0 || i == 20)
             {
-                *(dungeon + (i * 80) + j) = '-';
+                *(p) = '-';
             }
 
-            printf("%c", *(dungeon + (i * 80) + j));
+            int k;
+            for (k = 0; k < 6; k++)
+            {
+                Room room = rooms[k];
+                if (j >= room.x1 && j <= room.x2 && i >= room.y1 && i <= room.y2)
+                {
+                    *p = '.';
+                }
+            }
+
+            printf("%c", *(p));
         }
 
         printf("\n");
     }
 }
 
-void generate_rooms(Room *rooms)
+void generate_rooms(Room *rooms, int num_rooms, int x, int y)
 {
     srand(time(NULL));
-  
+
     int i;
-    for (i = 0; i < 6; i++)
+    for (i = 0; i < num_rooms; i++)
     {
         int _intersects = 0;
+        int _out_of_bounds = 0;
         do {
 
-        
-            rooms[i].x1 = rand() % 77 + 2;
-            rooms[i].y1 = rand() % 19 + 2;
-            rooms[i].x2 = rooms[i].x1 + rand() % 8 + 4;
-            rooms[i].y2 = rooms[i].y1 + rand() % 6 + 3;
+            rooms[i].x1 = rand() % (x - 4) + 2;
+            rooms[i].y1 = rand() % (y - 4) + 2;
+            rooms[i].x2 = rooms[i].x1 + rand() % 12 + 4;
+            rooms[i].y2 = rooms[i].y1 + rand() % 9 + 3;
 
             int j;
             for (j = (i - 1); j >= 0; j--)
@@ -79,14 +108,28 @@ void generate_rooms(Room *rooms)
                }
             }
 
-        } while(_intersects);  
+            _out_of_bounds = out_of_bounds(rooms[i], (x - 1), (y - 1));
+
+            attempts_to_generate++;
+        } while(_intersects || _out_of_bounds);  
     }
 
 }
 
+int out_of_bounds(Room room, int bound_x, int bound_y)
+{
+    if (room.x1 >= 2 && room.y1 >= 2 && room.x2 <= 77 && room.y2 <= 18)
+    {
+        //printf("(%d, %d) ", room.x2, room.y2);
+        return 0;
+    }
+
+    return 1;
+}
+
 int intersects(Room a, Room b)
 {
-    if (a.x1 > b.x2 || a.x2 < b.x1 || a.y1 > b.y2 || a.y2 < b.y1)
+    if (a.x1 > b.x2 + 1 || a.x2 < b.x1 - 1 || a.y1 > b.y2 + 1 || a.y2 < b.y1 - 1)
     {
         return 0;
     }
